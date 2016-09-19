0c761984ace2610035e7abaaa463a149c3436ceb
Amber Shen <amber95@host-2-17.ilcujsm.champaign.il.us.clients.pavlovmedia.com>
2016-09-19 00:14:25 -0500

    submission for menu and carousel

--- source_js/script.js
+++ source_js/script.js
@@ -1,16 +1,58 @@
-var divs = document.getElementsByTagName('div');
-for(var i=0; i<divs.length; i++) {
-  divs[i].addEventListener("click", highlightThis);
-  /*
-  divs[i].addEventListener("click", highlightThis, true);
-  divs[i].addEventListener("click", highlightThis, false);*/
+/*http://www.w3schools.com/howto/howto_js_slideshow.asp */
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n){
+	showSlides(slideIndex += n);
 }
 
-function highlightThis(event) {
-    //event.stopPropagation();
-
-    var backgroundColor = this.style.backgroundColor;
-    this.style.backgroundColor='yellow';
-    alert(this.className);
-    this.style.backgroundColor=backgroundColor;
+function currentSlide(n){
+	showSlides(slideIndex = n);
 }
+
+function showSlides(n){
+	var i;
+	var slides = document.getElementsByClassName("carousel-image");
+	if (n > slides.length){
+		slideIndex = 1;
+	}
+	if(n < 1){
+		slideIndex = slides.length;
+	}
+	for (i = 0; i < slides.length; i++){
+		slides[i].style.display = "none";
+	}
+	slides[slideIndex - 1].style.display = "block";
+}
+
+
+$(document).ready(function(){
+	$(document).scroll(function(){
+	/* for resizing navigation bar*/
+	if($(document).scrollTop() > 100){
+		$("header").removeClass("large").addClass("small");
+	}
+	else{
+		$("header").removeClass("small").addClass("large");
+	}
+	/* position indicator */
+	if($(document).scrollTop() < $('#home').offset().top){
+		$('.nav-item').removeClass("nav-curr");
+		$('#navHome').addClass("nav-curr");
+	}
+	if($(document).scrollTop() >= $('#home').offset().top && $(document).scrollTop() < $('#trends').offset().top){
+		$('.nav-item').removeClass("nav-curr");
+		$("#navTrends").addClass("nav-curr");
+	}
+	if($(document).scrollTop() >= $('#trends').offset().top && $(document).scrollTop() < $('#snapshot').offset().top){
+		$('.nav-item').removeClass("nav-curr");
+		$("#navSnapshot").addClass("nav-curr");
+	}
+	if($(document).scrollTop() >= $('#snapshot').offset().top){
+		$('.nav-item').removeClass("nav-curr");
+		$('#navContact').addClass("nav-curr");
+	}
+
+});
+});
